2cb563cccc0f1ffc138a923b97da8c6bdb976ce0
letthem <suyeon3662@naver.com>
2024-09-30 23:58:19 +0900

    [Design] color & font 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: {
-          DEFAULT: '#1BD7BE',
-        },
+        bg: '#F8F8F8',
+        main: '#1BD7BE',
+        mainBlack: '#2B2B2B',
+        subBlack: '#212121',
+        gray01: '#D9D9D9',
+        gray02: '#A6A6A6',
+        gray03: '#ECECEC',
+        gray04: '#666666',
+        gray05: '#BBBBBB',
+        gray06: '#F2F2F2',
+      },
+      fontFamily: {
+        nanum: ['"NanumSquare Neo"', 'sans-serif'],
       },
     },
   },
