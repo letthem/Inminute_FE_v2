7444ce928152275d2c6309307aee15b90e2e30fe
letthem <suyeon3662@naver.com>
2024-10-01 12:26:09 +0900

    [Rename] List페이지 -> Main페이지로 이름 변경

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,14 +1,14 @@
 import { FolderBar } from '@/components/FolderBar/FolderBar';
 import { NavBar } from '@/components/NavBar/NavBar';
 
-export const ListPage = () => {
+export const MainPage = () => {
   return (
     <>
       <div className="w-screen h-screen flex flex-row bg-bg">
         <FolderBar />
         <section className="flex flex-col w-[calc(100vw-280px)] h-full">
           <NavBar />
-          List 페이지입니다.
+          Main 페이지입니다.
         </section>
       </div>
     </>
