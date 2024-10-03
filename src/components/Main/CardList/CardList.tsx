937e28dbf5b6e0404b7716980d6a50f9ca0df5c0
letthem <suyeon3662@naver.com>
2024-10-04 00:23:45 +0900

    [Design] CardList 스크롤 시 검색창 아래에 그라데이션 추가

--- src/components/Main/CardList/CardList.tsx
+++ src/components/Main/CardList/CardList.tsx
@@ -13,18 +13,21 @@ interface CardListProps {
 
 export const CardList: React.FC<CardListProps> = ({ cards }) => {
   return (
-    <div className="overflow-y-auto scrollbar-hide">
-      <div className="ml-[72px] mr-[92px] mt-4 mb-10 grid grid-cols-3 gap-x-[28px] gap-y-6">
-        {cards.map((card, index) => (
-          <CardItem
-            key={index}
-            date={card.date}
-            title={card.title}
-            summary={card.summary}
-            folder={card.folder}
-          />
-        ))}
+    <>
+      <div className="absolute mt-[192px] h-4 w-full bg-gradient-to-b from-bg to-transparent " />
+      <div className="overflow-y-auto scrollbar-hide">
+        <div className="ml-[72px] mr-[92px] mt-4 mb-10 grid grid-cols-3 gap-x-[28px] gap-y-6">
+          {cards.map((card, index) => (
+            <CardItem
+              key={index}
+              date={card.date}
+              title={card.title}
+              summary={card.summary}
+              folder={card.folder}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
