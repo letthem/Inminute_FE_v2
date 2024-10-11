0112901efbc25951501fbcec126bf1f092525b2d
letthem <suyeon3662@naver.com>
2024-10-12 01:27:01 +0900

    [Feat] NewNoteModal UI 구현

--- src/components/Main/MainTopBar/NewNoteModal/FolderDropDown/FolderDropDown.tsx
+++ src/components/Main/MainTopBar/NewNoteModal/FolderDropDown/FolderDropDown.tsx
@@ -1,15 +1,15 @@
-import { useState, useEffect, useRef } from 'react';
-import down from '@/assets/webps/Main/downBlack.webp';
-import up from '@/assets/webps/Main/upBlack.webp';
+import React, { useEffect, useRef, useState } from 'react';
+import down from '@/assets/svgs/Main/downGray.svg';
+import up from '@/assets/svgs/Main/upGray.svg';
 import check from '@/assets/svgs/Main/check.svg';
 
-interface SortDropdownProps {
+interface FolderDropdownProps {
   options: string[];
   selectedOption: string;
   onOptionSelect: (option: string) => void;
 }
 
-export const SortDropdown: React.FC<SortDropdownProps> = ({
+export const FolderDropDown: React.FC<FolderDropdownProps> = ({
   options,
   selectedOption,
   onOptionSelect,
@@ -42,25 +42,36 @@ export const SortDropdown: React.FC<SortDropdownProps> = ({
   }, [isDropdownOpen]);
 
   return (
-    <div className="relative" ref={dropdownRef}>
+    <>
       <div
-        className="h-[22px] mr-6 flex items-center cursor-pointer"
+        tabIndex={0}
+        className="relative w-[408px] h-[76px] rounded-[10px] mt-6 cursor-pointer"
+        style={{ boxShadow: '0 0 0 1px #D9D9D9 inset' }}
+        onFocus={(e) => (e.target.style.boxShadow = '0 0 0 1px #2B2B2B inset')}
+        onBlur={(e) => (e.target.style.boxShadow = '0 0 0 1px #D9D9D9 inset')}
+        ref={dropdownRef}
         onClick={handleDropdownToggle}
       >
-        <span className="mr-2 font-medium text-mainBlack text-[13px]">{selectedOption}</span>
-        <img className="w-5 h-5" src={isDropdownOpen ? up : down} alt="toggle dropdown" />
+        <p className="font-[500] text-[11px] mt-[10px] ml-4 text-gray05">폴더 선택</p>
+        <img
+          src={isDropdownOpen ? up : down}
+          alt="toggle dropdown"
+          className="absolute w-5 h-5 top-10 right-3"
+        />
+        <span className="absolute pt-[6px] px-4 text-[15px] font-[500] text-mainBlack">
+          {selectedOption}
+        </span>
       </div>
-
       <div
-        className={`w-[120px] y-[100px] absolute top-full mt-3 right-6 bg-white rounded-[10px] shadow-[0_0_4px_0_rgba(0,0,0,0.25)] z-20 transition-all duration-200 ease-in-out overflow-hidden ${
+        className={`w-[408px] h-[122px] absolute top-[303px] bg-white rounded-[10px] shadow-[0_0_4px_0_rgba(0,0,0,0.25)] z-20 transition-all duration-200 ease-in-out overflow-hidden ${
           isDropdownOpen ? 'max-h-[200px] opacity-100' : 'max-h-0 opacity-0'
         }`}
       >
-        <ul className="mx-[6px] my-2">
+        <ul className="mx-[10px] mt-[7px]">
           {options.map((option) => (
             <li
               key={option}
-              className={`pl-2 py-[3px] text-[12px] font-medium cursor-pointer rounded flex items-center hover:bg-gray02 justify-between ${
+              className={`pl-2 h-[32px] mb-[6px] pt-[4px] pb-[5px] text-[14px] leading-[15px] font-medium cursor-pointer rounded flex items-center hover:bg-gray02 ${
                 option === selectedOption ? 'text-gray04' : 'text-mainBlack'
               }`}
               onClick={() => {
@@ -70,12 +81,13 @@ export const SortDropdown: React.FC<SortDropdownProps> = ({
             >
               {option}
               {option === selectedOption && (
-                <img className="w-[11px] mr-[14px]" src={check} alt="selected" />
+                <img className="w-[11px] ml-[12px]" src={check} alt="selected" />
               )}
             </li>
           ))}
         </ul>
       </div>
-    </div>
+      
+    </>
   );
 };
