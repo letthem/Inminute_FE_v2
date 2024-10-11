891f793e7a80c26b8d64a2bd0b66515c6ea21013
letthem <suyeon3662@naver.com>
2024-10-12 02:12:48 +0900

    [Feat] CardData가 없을 때 화면 구현

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -3,6 +3,7 @@ import { FolderBar } from '@/components/FolderBar/FolderBar';
 import { MainTopBar } from '@/components/Main/MainTopBar/MainTopBar';
 import { NavBar } from '@/components/NavBar/NavBar';
 import { CardList } from '@/components/Main/CardList/CardList';
+import noteMint from '@/assets/webps/Main/noteMint.webp';
 
 interface CardData {
   date: string;
@@ -144,13 +145,16 @@ export const MainPage = () => {
         <section className="flex flex-col w-[calc(100vw-280px)] h-full">
           <NavBar />
           <MainTopBar onSearch={setSearchQuery} onSort={setSortOption} />
-          <CardList cards={filteredCards} />
-          {/* <div className="flex flex-col items-center justify-center">
-            <img className="w-[147px] h-[154px] mt-[136px]" src={noteMint} alt="note" />
-            <p className="mt-[27.5px] text-mainBlack font-medium text-[15px]">
-              회의 노트를 추가해보세요 !
-            </p>
-          </div> */}
+          {filteredCards.length > 0 ? (
+            <CardList cards={filteredCards} />
+          ) : (
+            <div className="flex flex-col items-center justify-center">
+              <img className="w-[147px] h-[154px] mt-[136px]" src={noteMint} alt="note" />
+              <p className="mt-[27.5px] text-mainBlack font-medium text-[15px]">
+                회의 노트를 추가해보세요 !
+              </p>
+            </div>
+          )}
         </section>
       </div>
     </>
