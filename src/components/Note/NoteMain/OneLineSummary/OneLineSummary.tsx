97198f7c23c9eb5e9405d7e7c33dd93f829fd63a
letthem <suyeon3662@naver.com>
2024-10-12 12:35:39 +0900

    [Design] 한 줄 요약 최대 너비 수정

--- src/components/Note/NoteMain/OneLineSummary/OneLineSummary.tsx
+++ src/components/Note/NoteMain/OneLineSummary/OneLineSummary.tsx
@@ -1,7 +1,7 @@
 export const OneLineSummary = () => {
   return (
     <section className="mt-5 ml-12">
-      <div className="mr-[120px] max-w-[760px] rounded-[10px] bg-[#ECECEC]">
+      <div className="mr-[120px] max-w-[865px] rounded-[10px] bg-[#ECECEC]">
         <p className="font-[350] text-[13.5px] font-pretendard leading-[24.3px] py-[14px] px-[20px]">
           오프라인 마케팅 쪽은 이벤트와 프로모션에 집중하고 있는데 이번 주에 교체한 제품체험
           이벤트는 생각보다 많은 고객이 참여하여 성공적으로 마무리 되었고 다음 달에 대형 쇼핑몰에서
