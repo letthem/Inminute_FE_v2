7722ce556507bdf0e4b8379f4436afdb1e390f1c
letthem <suyeon3662@naver.com>
2024-10-16 23:33:44 +0900

    [Feat] 초기 note 화면 UI 구현

--- src/components/Note/NoteMain/NoteMain.tsx
+++ src/components/Note/NoteMain/NoteMain.tsx
@@ -4,17 +4,29 @@ import { ParticipantList } from '@/components/Note/NoteMain/ParticipantList/Part
 import { OneLineSummary } from '@/components/Note/NoteMain/OneLineSummary/OneLineSummary';
 import { SummaryBySpeakerList } from '@/components/Note/NoteMain/SummaryBySpeakerList/SummaryBySpeakerList';
 import { ToDoList } from '@/components/Note/NoteMain/ToDoList/ToDoList';
+import { useState } from 'react';
 
 export const NoteMain = () => {
+  const [showContent] = useState(false);
+
+  // 회의가 끝난 이후 handleShowContent 추후 호출 예정
+  // const handleShowContent = () => {
+  //   setShowContent(true);
+  // };
+
   return (
     <main className="flex flex-1 flex-col">
       <NoteTopBar />
       <NoteTitle />
       <div className="overflow-y-auto scrollbar-hide">
-        <ParticipantList />
-        <OneLineSummary />
-        <SummaryBySpeakerList />
-        <ToDoList />
+        {showContent && (
+          <>
+            <ParticipantList />
+            <OneLineSummary />
+            <SummaryBySpeakerList />
+            <ToDoList />
+          </>
+        )}
       </div>
     </main>
   );
--- src/components/Note/NoteMain/NoteTitle/NoteTitle.tsx
+++ src/components/Note/NoteMain/NoteTitle/NoteTitle.tsx
@@ -1,12 +1,29 @@
 import calendarBlack from '@/assets/webps/Note/calendarBlack.webp';
 import timeBlack from '@/assets/webps/Note/timeBlack.webp';
+import link from '@/assets/svgs/Note/link.svg';
+import mic from '@/assets/svgs/Note/mic.svg';
 
 export const NoteTitle = () => {
   return (
     <section className="flex flex-col">
-      <p className="text-[26px] font-bold mt-[30px] ml-12 mr-[120px] leading-[30px]">
-        브랜드 아이덴티티 전략 회의
-      </p>
+      <div className="flex justify-between items-center mt-[30px]">
+        <p className="text-[26px] font-bold ml-12 mr-[120px] leading-[30px]">
+          브랜드 아이덴티티 전략 회의
+        </p>
+        <div className="flex text-white text-[10.5px] leading-[18px]">
+          <div className="flex items-center w-[88px] h-[30px] mr-3 bg-mainBlack rounded-[3.2px] cursor-pointer">
+            <img className="w-[15px] h-[15px] ml-3 mr-[6px]" src={link} alt="link" />
+            <span className="font-[500]">링크 복사</span>
+          </div>
+          <div className="flex justify-center items-center w-[58px] h-[30px] mr-3 bg-main06 rounded-[3.2px] cursor-pointer">
+            <span className="font-[700]">ZOOM</span>
+          </div>
+          <div className="flex items-center w-[88px] h-[30px] mr-[27px] bg-mainBlack rounded-[3.2px] cursor-pointer">
+            <img className="w-[15px] h-[15px] ml-[12.3px] mr-[6.3px]" src={mic} alt="mic" />
+            <span className="font-[500]">회의 시작</span>
+          </div>
+        </div>
+      </div>
 
       <div className="flex ml-[49px] mt-[13px] items-center mb-2">
         <div className="flex items-center">
