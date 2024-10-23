af522cb3c11312fe386e7c2cf21b67985accb08d
letthem <suyeon3662@naver.com>
2024-10-23 13:07:12 +0900

    [Feat] Inminute 시작하기 버튼 UI

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -3,7 +3,7 @@ import { NavBar } from '@/components/NavBar/NavBar';
 import { Title } from '@/components/About/Title';
 import { MainFeature } from '@/components/About/MainFeature/MainFeature';
 import { SubFeature } from '@/components/About/SubFeature/SubFeature';
-
+import right from '@/assets/svgs/About/right.svg';
 export const AboutPage = () => {
   const [isVisible, setIsVisible] = useState(false);
 
@@ -23,6 +23,14 @@ export const AboutPage = () => {
         <Title />
         <MainFeature />
         <SubFeature />
+        <section className="bg-bg flex justify-center pb-[277px]">
+          <div className="bg-subBlack w-[374px] h-[98px] rounded-[60px] flex justify-center items-center cursor-pointer">
+            <span className="text-white font-[700] leading-[180%] text-[28px] mr-[14px]">
+              Inminute 시작하기
+            </span>
+            <img src={right} alt="right" className="w-[32px] h-[25px]" />
+          </div>
+        </section>
       </main>
     </>
   );
