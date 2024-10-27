3942118e18aa04723b0aeaa072240e0234d83822
letthem <suyeon3662@naver.com>
2024-10-28 00:57:45 +0900

    [Design] AboutTitle 로티 반복재생

--- src/components/About/Title.tsx
+++ src/components/About/Title.tsx
@@ -5,7 +5,7 @@ export const Title = () => {
   return (
     <article className="bg-sub2Black">
       <div className="s10:mx-[117px] mt-[144px] s10:w-[967px] mb-[292px]">
-        <Lottie animationData={aboutTitle} loop={false} />
+        <Lottie animationData={aboutTitle} />
       </div>
     </article>
   );
