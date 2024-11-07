b5920967e42bbe299cfbfc54906fb4130427e6ff
letthem <suyeon3662@naver.com>
2024-11-07 17:41:04 +0900

    [Feat] 일정 추가 버튼 UI 구현

--- src/components/Calendar/YearAndMonth/YearAndMonth.tsx
+++ src/components/Calendar/YearAndMonth/YearAndMonth.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { format } from 'date-fns';
 import leftBlack from '@/assets/webps/Calendar/leftBlack.webp';
 import rightBlack from '@/assets/webps/Calendar/rightBlack.webp';
+import calendarMint from '@/assets/webps/Calendar/calendarMint.webp';
 
 interface YearAndMonthProps {
   currentMonth: Date;
@@ -14,21 +15,30 @@ export const YearAndMonth: React.FC<YearAndMonthProps> = ({
   onPrevMonth,
   onNextMonth,
 }) => (
-  <div className="w-[270px] flex justify-between items-center mb-9">
-    <img
-      onClick={onPrevMonth}
-      className="w-[22px] h-[22px] cursor-pointer"
-      src={leftBlack}
-      alt="prev month"
-    />
-    <h2 className="text-mainBlack text-[32px] font-[750] leading-[35px]">
-      {format(currentMonth, 'yyyy.MM')}
-    </h2>
-    <img
-      onClick={onNextMonth}
-      className="w-[22px] h-[22px] cursor-pointer"
-      src={rightBlack}
-      alt="next month"
-    />
+  <div className="w-full flex justify-between">
+    <div className="flex flex-1 justify-center mb-9">
+      <div className="w-[270px] flex justify-between items-center ">
+        <img
+          onClick={onPrevMonth}
+          className="w-[22px] h-[22px] cursor-pointer"
+          src={leftBlack}
+          alt="prev month"
+        />
+        <h2 className="text-mainBlack text-[32px] font-[750] leading-[35px]">
+          {format(currentMonth, 'yyyy.MM')}
+        </h2>
+        <img
+          onClick={onNextMonth}
+          className="w-[22px] h-[22px] cursor-pointer"
+          src={rightBlack}
+          alt="next month"
+        />
+      </div>
+    </div>
+    {/* 일정 추가 */}
+    <div className="w-[101px] mb-[30px] h-[38px] bg-mainBlack rounded-[4px] px-[14px] py-2 gap-2 flex items-center cursor-pointer">
+      <img src={calendarMint} alt="calendar mint" className="w-4 h-4" />
+      <span className="text-white text-[12px] font-[500] leading-[22px]">일정 추가</span>
+    </div>
   </div>
 );
