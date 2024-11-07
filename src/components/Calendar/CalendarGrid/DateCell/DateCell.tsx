030ff0a820a1f4b019905701fd304c122515bca6
letthem <suyeon3662@naver.com>
2024-11-07 17:26:27 +0900

    [Feat] 추가된 회의 UI 구현

--- src/components/Calendar/CalendarGrid/DateCell/DateCell.tsx
+++ src/components/Calendar/CalendarGrid/DateCell/DateCell.tsx
@@ -31,7 +31,7 @@ export const DateCell: React.FC<DateCellProps> = ({
 
   return (
     <div
-      className={`w-[14.3%] h-[130px] border-[0.3px] flex flex-col cursor-pointer
+      className={`w-[14.3%] h-[130px] bg-white border-[0.3px] flex flex-col cursor-pointer
       ${isFirstRow && isFirstCol ? 'rounded-tl-[10px]' : ''} 
       ${isFirstRow && isLastCol ? 'rounded-tr-[10px]' : ''} 
       ${isLastRow && isFirstCol ? 'rounded-bl-[10px]' : ''} 
@@ -55,9 +55,18 @@ export const DateCell: React.FC<DateCellProps> = ({
         <div className="h-[1.4px] bg-black ml-4 mt-[-1px]" style={{ width: dateWidth }} />
       )}
       {isSpecial && (
-        <div className="flex flex-col items-start p-1">
-          <p className="text-xs text-gray-500">~~</p>
-          <p className="text-xs text-gray-500">~~~</p>
+        <div className="flex flex-col mt-[5px] ml-[10px] mr-1 gap-[6px]">
+          <div className="flex w-full h-6 bg-[#EAFBEC] rounded-[4px] overflow-hidden items-center">
+            <p className="ml-[8.3px] text-[12px] font-[500] leading-[22px] text-[#489D06]">
+              TF팀 회의
+            </p>
+          </div>
+          <div className="flex w-full h-6 bg-[#FCF3FD] rounded-[4px] overflow-hidden items-center">
+            <div className="w-1 h-full bg-[#FF94A4]" />
+            <p className="ml-[8.3px] text-[12px] font-[500] leading-[22px] text-[#E7546A]">
+              해커톤 정기회의
+            </p>
+          </div>
         </div>
       )}
     </div>
