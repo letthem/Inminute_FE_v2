5867c7ec24b6d418fd95229db95c70e27310425b
letthem <suyeon3662@naver.com>
2024-11-12 16:49:41 +0900

    [Test] 테스트용 탈퇴 제작

--- src/components/About/Title.tsx
+++ src/components/About/Title.tsx
@@ -1,11 +1,23 @@
 import Lottie from 'lottie-react';
 import aboutTitle from '@/assets/lotties/aboutTitle.json';
+import { deleteMember } from '@/apis/Member/deleteMember';
 
 export const Title = () => {
+  const handleDeleteMember = async () => {
+    await deleteMember();
+    alert('탈퇴가 완료되었습니다.');
+  };
+
   return (
     <article className="bg-sub2Black">
       <div className="s10:mx-[117px] mt-[144px] s10:w-[967px] mb-[292px]">
         <Lottie animationData={aboutTitle} />
+        <div
+          className="w-8 h-[22px] bg-gray06 rounded-md cursor-pointer"
+          onClick={handleDeleteMember}
+        >
+          탈퇴
+        </div>
       </div>
     </article>
   );
