389a1d6ee6f5bf3cc540b8d8a5bdd84d6f152d1a
letthem <suyeon3662@naver.com>
2024-11-24 15:10:37 +0900

    [Design] Title, TextItem 반응형 수정

--- src/components/About/MainFeature/TextItem/TextItem.tsx
+++ src/components/About/MainFeature/TextItem/TextItem.tsx
@@ -17,9 +17,14 @@ export const TextItem: React.FC<TextItemProps> = ({ feature, desc1, desc2 }) => 
   }, []);
 
   return (
-    <div data-aos="fade-up" className="mt-[102px] mb-[1000px] h-[150px]">
-      <p className="text-center s1000:text-start text-[28px] tab:text-[36px] s1000:text-[23px] s1200:text-[32px] s1300:text-[40px] s14:text-[35px] s16:text-[40px] leading-[76px] font-bold font-nanumv text-white">{feature}</p>
-      <div className="text-center s1000:text-start text-[14px] tab:text-[19px] s1000:text-[13px] s1200:text-[15px] s1300:text-[19px] s14:text-[16px] s16:text-[19px] text-gray05 leading-[170%] font-pretendard font-[400] mt-[9px]">
+    <div
+      data-aos="fade-up"
+      className="mt-[102px] mb-[300px] s510:mb-[500px] s960:mb-[1000px] h-[150px]"
+    >
+      <p className="text-center s960:text-start text-[28px] tab:text-[36px] s960:text-[23px] s1200:text-[32px] s1300:text-[40px] s14:text-[35px] s16:text-[40px] leading-[76px] font-bold font-nanum text-white">
+        {feature}
+      </p>
+      <div className="text-center s960:text-start text-[14px] tab:text-[19px] s960:text-[13px] s1200:text-[15px] s1300:text-[19px] s14:text-[16px] s16:text-[19px] text-gray05 leading-[170%] font-pretendard font-[400] mt-[9px]">
         <p>{desc1}</p>
         <p>{desc2}</p>
       </div>
