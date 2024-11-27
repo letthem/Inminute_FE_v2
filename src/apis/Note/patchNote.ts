b9f0079012cdff99a31b26e2ac6d78f645f31d10
letthem <suyeon3662@naver.com>
2024-11-28 00:09:36 +0900

    [Feat] 한 줄 요약 수정 PATCH API 연결

--- src/apis/Note/patchNote.ts
+++ src/apis/Note/patchNote.ts
@@ -8,3 +8,30 @@ export const checkToDo = async (toDoId: number, content: string, isDone: boolean
     throw error;
   }
 };
+
+export const patchNoteTitle = async (noteId: number, name: string) => {
+  try {
+    await instance.patch(`notes/${noteId}`, { name });
+  } catch (error) {
+    console.error('note 제목 수정 중 에러 발생:', error);
+    throw error;
+  }
+};
+
+export const patchOneLineSummary = async (noteId: number, summary: string) => {
+  try {
+    await instance.patch(`notes/${noteId}`, { summary });
+  } catch (error) {
+    console.error('note 한 줄 요약 수정 중 에러 발생:', error);
+    throw error;
+  }
+};
+
+export const patchSummaryBySpeaker = async (noteJoinMemberId: number, summary: string) => {
+  try {
+    await instance.patch(`notes/join-member/${noteJoinMemberId}`, { summary });
+  } catch (error) {
+    console.error('note 화자별 요약 수정 중 에러 발생:', error);
+    throw error;
+  }
+};
